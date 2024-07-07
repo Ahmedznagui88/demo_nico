d45232ad01b12836586485172997200943996d87
Ahmed Znagui <amed.bob88@gmail.com>
2024-07-07 19:20:53 +0200

    revoke gmKey

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,19 @@ import Navbar from "./components/NavBar/MyNavbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Servizi from "./components/Servizi/MyComponent";
-import DoveSiamo from "./components/DoveSiamo/Location";
-import { useGlobalContext } from "./components/context";
+
 
 function App() {
-  const prova = useGlobalContext();
+
   return (
     <Router className="App">
       <Navbar />
       <Header />
       <Servizi />
-      <DoveSiamo />
-      {/*       <Routes>
+            <Routes>
         <Route path="/" element={<Header />} />
         <Route path="/" element={<Servizi />} />
-        <Route path="/dovesiamo" element={<DoveSiamo />} />
-      </Routes> */}
+      </Routes>
     </Router>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
         </div>
         </div>
         <div className='col-12 col-md-6'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vestibulum ultricies diam. Morbi interdum egestas sapien, in volutpat lacus euismod ac. Maecenas mollis scelerisque nulla et laoreet. Vivamus non arcu fringilla, pulvinar magna eu, imperdiet magna. Integer et tellus congue, volutpat leo vel, dapibus velit. Suspendisse mattis lacus eget lorem fermentum egestas. Fusce eget nunc lectus. Curabitur gravida porta orci ac luctus. Phasellus pellentesque, turpis ut finibus rhoncus, leo arcu ullamcorper est, tristique volutpat orci turpis eget dolor. Nunc dignissim urna eu turpis lobortis, at blandit est commodo. Integer ut purus vitae lorem mattis varius. Nulla facilisi. Donec a vehicula dolor. Donec at mi malesuada arcu porttitor semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut id neque vitae felis interdum consequat.
+          Hi everyone this is a header
         </div>
       </div>
     </div>
