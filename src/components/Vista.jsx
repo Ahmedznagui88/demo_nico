cbe7601ceb268a177c92503638b6f66e0a131723
Ahmed Znagui <amed.bob88@gmail.com>
2024-07-10 02:56:08 +0200

    periodic table json

--- src/components/Vista.jsx
+++ src/components/Vista.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const url = "https://github.com/mdakram09/Periodic-Table-API-Made-Using-NodeJS-and-ExpressJS/blob/main/api/routes/db.json";
 
-const Vista= () => {
-
-    return (
-      <>
-      <h1>ciao ahmed</h1>
-      
-      </>
-      
-    );
+const Fetch = () => {
+  const [element, setElement] = useState([]);
+  const getData = async () => {
+    const response = await axios.get(url);
+    console.log(response);
   };
+  useEffect(() => {}, []);
+  getData();
+  return (
+    <>
+      <ul>
+        {element.map((item) => {
+          return <li>
+            name
+          </li>;
+        })}
+      </ul>
+    </>
+  );
+};
 
-  export default Vista;
+export default Fetch
+
+
